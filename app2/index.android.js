154cddc206acfb269973302865ea47ac056a31b3
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-02-19 23:01:50 -0300

    Incluindo imagens

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,75 +1,27 @@
 //Imports
 import React from 'react';
-import {Text, AppRegistry, View, TouchableOpacity } from 'react-native';
+import {AppRegistry, View, Image, Text } from 'react-native';
 
 //Formatações
 const Estilos = {
-  /*estiloTexto: {
-    fontSize: 20,
-    backgroundColor: '#08509B',
-    width: 60, 
-    height: 60,
-
-    color: '#fff',
-    paddingTop: 15,
-    textAlign: 'center'
-    textDecorationLine: 'underline line-through',
-    fontWeight: 'bold' //100 e 900
-    fontStyle: 'italic'
-    shadowColor: '#000',
-    shadowOffset: {width:0, height:2},
-    shadowOpacity: 0.4
-    paddingLeft: 10,
-    textAlign: 'center',
-  
-    flex: 0,
-    flexDirection: 'row'
-  },*/
-
   principal: {
-    height: 150,
-    paddingTop: 0,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black'
+    paddingTop: 40
   },
-
-  botao: {
-    backgroundColor: '#48BBEC',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 1,
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: {width:0, height: 2},
-    shadowOpacity: 0.4,
-    width: 250
-  },
-
-  textoBotao:{
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
+  imagem: {
+    justifyContent: 'flex-start',
+    padding: 5,
   }
-
 };
-
-const botaoPressionado = () => {
-  alert('Botão pressionado');
-}
 
 //Criar o componente
 const App = () => {
-  const { principal, botao, textoBotao } = Estilos;
+  const { principal, imagem } = Estilos;
 
   return (
       <View style= { principal }>
-        
-        <TouchableOpacity style= { botao } onPress={ botaoPressionado }>
-          <Text style= { textoBotao }>Clique Aqui</Text>
-        </TouchableOpacity>
-      
+        <Image style= { imagem } source={ require('./images/uvas.png') } >
+          <Text>Legenda para foto</Text>
+        </Image>
       </View>
     );
 };
