fbb252603118849fc8dbc7dc732efce36ba5ead5
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-02-21 10:58:24 -0300

    Criação do compenente listaItens

---
+++ app4/src/components/ListaItens.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class ListaItens extends Component {
+  render() {
+    return (
+      <Text>Componente teste</Text>
+    );
+  }
+}
+
+
