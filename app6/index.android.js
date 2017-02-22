a3d621ae4f566b330ebf085e584a06d958ed5557
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-02-22 10:06:34 -0300

    inicio projeto 6

---
+++ app6/index.android.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+export default class app6 extends Component {
+  render() {
+    return (
+      <View>
+       
+      </View>
+    );
+  }
+}
+
+
+
+AppRegistry.registerComponent('app6', () => app6);
