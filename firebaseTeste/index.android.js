3f86978a4bff3f94512f7acf40121d1412e76621
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-02-23 20:13:19 -0300

    inclusão do projeto do firebase e configuração de deploy signed

--- firebaseTeste/index.android.js
+++ firebaseTeste/index.android.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, Button } from 'react-native';
+import { AppRegistry, Text, View, Button, StyleSheet } from 'react-native';
 
 import firebase from 'firebase';
 
 export default class firebaseTeste extends Component {
   
-constructor(props){
-  super(props);
-  this.state = { pontuacao : 0 }
-}
 
   componentWillMount() {
     const config = {
@@ -21,56 +17,135 @@ constructor(props){
     firebase.initializeApp(config);
   }
 
-  salvarDados(){
-    var funcionarios = firebase.database().ref('funcionarios');
-    // database.ref('pontuacao').remove();
-    // funcionarios.child('002').remove();
-    // funcionarios.child('002').child('idade').set('12');
-    // funcionarios.push().child('nome').set('Dudy');
-    funcionarios.push().set (
-        {
-          nome: 'Crinaço',
-          altura: '2,10',
-          peso: '256'
-        }
-      );
+  cadastrarUsuario(){
+    var email = "edujunque@gmail.com";
+    var senha = "dudy123";
     
-  }
+   const usuario = firebase.auth();
+    usuario.createUserWithEmailAndPassword(email, senha)
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
-  listarDados(){
-     var pontuacao = firebase.database().ref('pontuacao');
-     pontuacao.on('value', (snapshot) => { 
-        var pontos = snapshot.val();
-        this.setState({ pontuacao : pontos});
+        if (error.code == 'auth/weak-password'){
+           alert('Crianço!!! essa senha é horrivel, melhora ela ai pelo menos 6 caracteres');
+        }
+
+       
       });
   }
 
+  verificarUsuarioLogado(){
+     const usuario = firebase.auth();
+     const usuarioAtual = usuario.currentUser;
+      usuario.onAuthStateChanged(
+        (usuarioAtual) => {
+          if( usuarioAtual ){
+            alert('Usario logado')
+          } else {
+            alert('Usario não logado')
+
+          }
+        }
+      );
+  } 
+
+  deslogarUsuario(){
+     const usuario = firebase.auth();
+      usuario.signOut();
+  } 
+
+  logarUsuario(){
+    var email = 'edujunque@gmail.com';
+    var senha = 'dudy123'
+    const usuario = firebase.auth();
+
+    usuario.signInWithEmailAndPassword(email, senha)
+    .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (error.code == 'auth/wrong-password'){
+         alert('Crianço!!! essa senha ou email estão erradas');
+       }
+
+       
+     });
+  }
+
+  // salvarDados(){
+  //   var funcionarios = firebase.database().ref('funcionarios');
+  //   // database.ref('pontuacao').remove();
+  //   // funcionarios.child('002').remove();
+  //   // funcionarios.child('002').child('idade').set('12');
+  //   // funcionarios.push().child('nome').set('Dudy');
+  //   funcionarios.push().set (
+  //       {
+  //         nome: 'Crinaço',
+  //         altura: '2,10',
+  //         peso: '256'
+  //       }
+  //     );
+    
+  // }
+
+  // listarDados(){
+  //    var pontuacao = firebase.database().ref('pontuacao');
+  //    pontuacao.on('value', (snapshot) => { 
+  //       var pontos = snapshot.val();
+  //       this.setState({ pontuacao : pontos});
+  //     });
+  // }
+
   render() {
     return (
-      <View>
+      <View style={styles.botao}>
         <Text>
-          {this.state.pontuacao}
+         
         </Text>
-         <Button 
-            onPress={ () => { this.salvarDados(); } }
-            title= 'Salvar dados'
+         <Button style={{flex:1}}
+            onPress={ () => { this.cadastrarUsuario(); } }
+            title= 'Cadastrar usuario'
             color= '#841584'
-            accessibilityLabel='Salvar dados'
+            accessibilityLabel='Cadastrar usuario'
           />
 
-          <Button 
-            onPress={ () => { this.listarDados(); } }
-            title= 'Listar dados'
+          <Button style={{flex:1}}
+            onPress={ () => { this.verificarUsuarioLogado(); } }
+            title= 'Verifica usuario logado'
             color= '#841584'
-            accessibilityLabel='Listar dados'
+            accessibilityLabel='Verifica usuario logado'
           />
+
+          <Button style={{flex:1}}
+          onPress={ () => { this.deslogarUsuario(); } }
+          title= 'Deslogar usuario'
+          color= '#841584'
+          accessibilityLabel='Deslogar usuario'
+          /> 
+
+          <Button style={{flex:1}}
+          onPress={ () => { this.logarUsuario(); } }
+          title= 'Logar usuario'
+          color= '#841584'
+          accessibilityLabel='Logar usuario'
+          />
+
+
       </View>
     );
   }
 }
 
-// const styles=StyleSheet.create({
- 
-// });
+const styles=StyleSheet.create({
+ botao: {
+  margin: 10,
+  //width: 10
+  justifyContent: 'space-around',
+  flex: 5
+ }
+});
 
 AppRegistry.registerComponent('firebaseTeste', () => firebaseTeste);
